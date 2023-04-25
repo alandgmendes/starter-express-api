97d2c76c5890fddaefacb6be053ee3c41d0bea4a
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2023-04-25 19:48:43 -0300

    added verif

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ app.post("/login", (request, response) => {
 
 // free endpoint
 app.get("/free-endpoint", (request, response) => {
-  response.json({ message: "You are free to access me anytime" });
+  response.json({ message: "You are arara" });
 });
 
 // authentication endpoint
